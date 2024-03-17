1e1e4c39cf619d134ecc0ebdd171b8cd7f8395f5
kumarharsh3783 <60178988+kumarharsh3783@users.noreply.github.com>
2024-03-17 18:16:43 +0530

    adding adc driver code

--- src/ADC_Driver.h
+++ src/ADC_Driver.h
@@ -9,8 +9,34 @@
 #define ADC_DRIVER_H_
 
 /*********************************************************************
+ * Includes
+ *********************************************************************/
+#include "stm32f10x.h"
+#include "external.h"
+
+/*********************************************************************
  * Macros Definitions
  ********************************************************************/
+#define 	adc_mod1			1u
+#define 	adc_mod2			2u
+#define 	adc_mod3			3u
+
+#define 	APB2ENR_ADC1EN		0x0200
+#define 	APB2ENR_ADC2EN		0x0400
+#define		APB2ENR_ADC3EN		0x8000
+
+#define 	CFGR_ADCPRE_DIV2	(0u)
+#define		CFGR_ADCPRE_DIV4	(1 << 14)
+#define 	CFGR_ADCPRE_DIV6	(1 << 15)
+#define 	CFGR_ADCPRE_DIV8	((1 << 15) | (1 << 14))
+
+#define 	CR2_ADON			(1 << 0)
+#define		CR2_CONT			(1 << 1)
+#define 	CR2_CAL				(1 << 2)
+#define 	CR2_RSTCAL			(1 << 3)
+#define 	CR2_ALIGN			(1 << 11)
+#define 	CR2_LEFT_ALIGN		(1 << 11)
+#define 	CR2_RIGHT_ALIGN		~(1 << 11)
 
 /*********************************************************************
  * Variable Declarations
@@ -20,5 +46,11 @@
  * Function Declarations
  *********************************************************************/
 float map(long x, long in_min, long in_max, long out_min, long_out_max);
+
+void adcInit(unsigned char);
+void adcPeripheralEnable(unsigned char);
+void adcDeInit(unsigned char);
+void adcPeripheralDisable(unsigned char);
+unsigned int adcStartConversion(unsigned char);
 
 #endif /* ADC_DRIVER_H_ */
